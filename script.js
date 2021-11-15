023754ebfb434342d2701a7bc2bbaed90f4e31c4
MyUsername <my@email.com>
2021-11-15 15:04:10 +0100

    commit

--- script.js
+++ script.js
@@ -1,1 +1,105 @@
 // 1. creare un array di contatti da stampare in pagina
+
+var boolzapp = new Vue (
+    {
+        el: "#container",
+        data: {
+            contacts: [
+                {
+                    name: "Michele",
+                    avatar: "img/avatar_1.jpg",
+                    visible: "true",
+                    message: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            text: "Hai portato a spasso il cane?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 15:50:00",
+                            text: "Ricordati di dargli da mangiare",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 16:15:22",
+                            text: "Tutto fatto!",
+                            status: "received"
+                        }
+                    ],
+
+                    name: "Fabio",
+                    avatar: "img/avatar_2.jpg",
+                    visible: "true",
+                    message: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            text: "Hai portato a spasso il cane?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 15:50:00",
+                            text: "Ricordati di dargli da mangiare",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 16:15:22",
+                            text: "Tutto fatto!",
+                            status: "received"
+                        }
+                    ],
+
+                    name: "Samuele",
+                    avatar: "img/avatar_3.jpg",
+                    visible: "true",
+                    message: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            text: "Hai portato a spasso il cane?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 15:50:00",
+                            text: "Ricordati di dargli da mangiare",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 16:15:22",
+                            text: "Tutto fatto!",
+                            status: "received"
+                        }
+                    ],
+
+                    name: "Mario",
+                    avatar: "img/avatar_4.jpg",
+                    visible: "true",
+                    message: [
+                        {
+                            date: "10/01/2020 15:30:55",
+                            text: "Hai portato a spasso il cane?",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 15:50:00",
+                            text: "Ricordati di dargli da mangiare",
+                            status: "sent"
+                        },
+
+                        {
+                            date: "10/01/2020 16:15:22",
+                            text: "Tutto fatto!",
+                            status: "received"
+                        }
+                    ],
+                }
+            ]
+        }
+            
+    }
+);
